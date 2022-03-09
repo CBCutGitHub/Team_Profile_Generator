6bf0d9c6d41c670f1c6b8e4f912e36f5e57ea4c1
MDemp <mdempsey.cbc.ut@gmail.com>
2022-03-08 18:33:54 -0600

    Updated html template.

--- src/page-template.js
+++ src/page-template.js
@@ -101,7 +101,7 @@ module.exports = team => {
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My EXPERT Team</h1>
+                <h1 class="text-center">Webservices and Database Team</h1>
             </div>
         </div>
     </div>
